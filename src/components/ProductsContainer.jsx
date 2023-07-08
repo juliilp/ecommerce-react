4d79be2c417182ec6d9a083127932a29e0b5abf0
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-07-08 16:23:09 -0300

    render visible fixed

--- src/components/ProductsContainer.jsx
+++ src/components/ProductsContainer.jsx
@@ -1,27 +1,25 @@
-import React, { useEffect } from "react";
-import { useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import { StoreContext } from "../context/ContextProvider";
 import CardProducts from "./CardProducts";
+
 export default function ProductsContainer() {
   const { allProductRender } = useContext(StoreContext);
-  const render = [];
 
-  allProductRender.map((p) => p.visible === true && render.push(p));
+  const render = allProductRender.filter((p) => p.visible === "true");
+  console.log(render);
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:justify-self-center">
-      {render.map((producto, key) => {
-        return (
-          <CardProducts
-            image={producto.image}
-            title={producto.title}
-            price={producto.price}
-            category={producto.category}
-            id={producto.id}
-            key={key}
-            producto={producto}
-          />
-        );
-      })}
+      {render.map((producto, key) => (
+        <CardProducts
+          image={producto.image}
+          title={producto.title}
+          price={producto.price}
+          category={producto.category}
+          id={producto.id}
+          key={key}
+          producto={producto}
+        />
+      ))}
     </section>
   );
 }
