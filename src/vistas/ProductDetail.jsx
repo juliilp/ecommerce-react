ce9482b074d1d8cb70593db7132fd8bb6ca4907f
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-07-09 02:10:59 -0300

    mejoré detalle

--- src/vistas/ProductDetail.jsx
+++ src/vistas/ProductDetail.jsx
@@ -64,7 +64,7 @@ export default function ProductDetail() {
   };
 
   return (
-    <section className="flex w-full flex-col md:flex-row justify-center items-center mt-[5vh] px-10 mb-[100px] md:mb-0 md:px-0 md:gap-12 font-poppins xl:gap-40">
+    <section className="flex w-full flex-col mt-[15vh] md:flex-row justify-center items-center  px-10 mb-24 md:mt-[10vh] md:px-0 md:gap-20 font-poppins xl:gap-40">
       {product ? (
         <>
           <div className="mt-[5vh] border border- p-8 flex justify-center items-center  ">
@@ -75,13 +75,13 @@ export default function ProductDetail() {
             />
           </div>
           <div className="flex flex-col gap-8 md:gap-4 xl:w-[400px] xl:justify-center xl:gap-6">
-            <h1 className="font-bold w-[200px] mx-auto text-[20px] mt-6 mb-3 md:w-[250px] md:text-[25px] xl:w-[350px] xl:text-[30px]">
+            <h1 className="font-bold w-[200px] mx-auto text-[20px] mt-6 mb-3 md:w-[350px] md:text-[25px] xl:w-[350px] xl:text-[30px]">
               {product.title}
             </h1>
             <p className="text-[#8E8A8C] w-[295px] xl:w-[375px] ">
               {product.description}
             </p>
-            <div className="flex flex-col  w-[295px] ">
+            <div className="flex flex-col  w-[295px] md:mb-6">
               <span className="text-[16px] font-bold">
                 <span className="text-[20px] md:text-[25px] xl:text-[30px]">
                   ${product.price}
@@ -96,29 +96,34 @@ export default function ProductDetail() {
                 Suggested payments with 6 months special financing
               </span>
             </div>
-            <div className=" flex gap-4 items-center md:gap-6 xl:gap-10 ">
-              <div className="flex w-[146px] h-max items-center gap-6  px-7 py-3 border border-gray-400 rounded-full bg-[#F6F5F6]  ">
+            <div className=" flex gap-4 items-center md:gap-6 xl:gap-10 md:mb-6 ">
+              <div className="flex w-[146px] h-max items-center gap-6  px-7 py-3  rounded-full bg-[#F6F5F6]  ">
                 <IoMdRemove
                   className="cursor-pointer"
                   onClick={() => handlerRestaAmountDetail(id)}
+                  size={25}
                 />
-                <span>{product.amount}</span>
+                <span className="font-semibold text-lg">{product.amount}</span>
                 <IoMdAdd
                   className="cursor-pointer"
                   onClick={handlerSumaAmountDetail}
+                  size={25}
                 />
               </div>
               <div className="flex flex-col">
-                <span>
-                  Only{" "}
-                  <span className="text-[#F7AE7B] font-semibold">12 item</span>{" "}
+                <span className="font-semibold">
+                  Only
+                  <span className="text-[#F7AE7B] font-bold">
+                    {" "}
+                    12 item
+                  </span>{" "}
                   left!
                 </span>
-                <span>Don't miss it</span>
+                <span className="font-semibold">Don't miss it</span>
               </div>
             </div>
             <div className="flex gap-4">
-              <button className="mx-auto bg-red-500 rounded-full py-3 px-9 text-white font-semibold xl:py-5 xl:px-16 ">
+              <button className="mx-auto bg-red-500 rounded-full py-4 px-16 text-white font-semibold xl:py-5 xl:px-16 md:mb-6 ">
                 Buy Now
               </button>
             </div>
@@ -129,14 +134,18 @@ export default function ProductDetail() {
                   <span className="font-semibold">Free Delivery</span>
                 </div>
 
-                <span>Enter your Postal code for delivery Availability</span>
+                <span className="text-[#8e8A8C]">
+                  Enter your Postal code for delivery Availability
+                </span>
               </div>
               <div className="flex flex-col gap-1">
                 <div className="flex gap-1 items-center">
                   <FaMotorcycle size={35} color="#FC954B" />
-                  <span className="font-semibold">Return Delivery</span>
+                  <span className="font-semibold ">Return Delivery</span>
                 </div>
-                <span>sapn 30days Delivery Return. Details</span>
+                <span className="text-[#8e8A8C]">
+                  sapn 30days Delivery Return. Details
+                </span>
               </div>
             </div>
           </div>
