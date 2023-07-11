7ced1387426b4f304eb469d4e6ff952a50c2238c
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-07-11 00:17:04 -0300

    Eliminé carousel

--- src/components/MainHome.jsx
+++ src/components/MainHome.jsx
@@ -1,14 +1,5 @@
-import { Link } from "react-router-dom";
-import Carousel from "./Carousel";
-import HomeImagen from "../assets/imgHomeMain.jpg";
+import React from "react";
+
 export default function MainHome() {
-  // const { handlerMenu } = useContext(StoreContext);
-  return (
-    // w-full h-[90vh] mt-[10vh] lg:flex lg:justify-center
-    <main className="">
-      <Link to="/dashboard">
-        <h1>Dashboard</h1>
-      </Link>
-    </main>
-  );
+  return <div>MainHome</div>;
 }
